1fd9d261f1f1f44eca5e66a7530ad7dbdff1109e
James Doyley <jdoyley@gmail.com>
2017-02-23 08:39:02 +0000

    Add comments

--- lib/agent_counter.ex
+++ lib/agent_counter.ex
@@ -1,17 +1,26 @@
 defmodule AgentCounter do
   def start(initial_value) do
+    # Starting an agent just requires giving it a function that returns the
+    # agent's initial value.
     Agent.start(fn -> initial_value end)
   end
 
   def get_value(pid) do
+    # To get the value, we pass a function that receives the agent's state and
+    # returns whatever we want - in our case, we want to wrap the value in an
+    # `:ok` 2-tuple.
     Agent.get(pid, fn(x) -> {:ok, x} end)
   end
 
   def increment(pid) do
+    # To increment, we send a function that tells the agent what to do with its
+    # state.
     Agent.update(pid, fn(x) -> x + 1 end)
   end
 
   def decrement(pid) do
+    # To decrement, we send a function that tells the agent what to do with its
+    # state.
     Agent.update(pid, fn(x) -> x - 1 end)
   end
 end
--- lib/counter.ex
+++ lib/counter.ex
@@ -5,6 +5,12 @@ defmodule Counter do
 
   def loop(value) do
     receive do
+      # In our loop we'll expect to be told who a message is from, a ref that is
+      # unique to their request, and a term that tells us what to do - in this
+      # case, get the value of the counter.
+      #
+      # We'll send the value back with the ref so they know it's the appropriate
+      # response, and then we'll tail-call with the existing value
       { from, ref, :get_value } ->
         send(from, {:ok, ref, value})
         loop(value)
@@ -15,6 +21,7 @@ defmodule Counter do
     end
   end
 
+  # Then we just make the nice function to interact with our process loop
   def get_value(pid) do
     ref = make_ref()
     send(pid, { self(), ref, :get_value })
